9782e01a85385a40a09ef96c453faeb6e7cfec3c
yoonjeong <cyj199637@gmail.com>
2021-08-02 01:43:59 +0900

    first setting

---
+++ src/main/java/com/j2kb/minipetpee/MinipetpeeApplication.java
@@ -0,0 +1,13 @@
+package com.j2kb.minipetpee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipetpeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinipetpeeApplication.class, args);
+    }
+
+}
