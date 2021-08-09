ba64a025599f4a67b0ed4c87803590df58780914
yoonjeong <cyj199637@gmail.com>
2021-08-10 00:48:48 +0900

    <refactor>aws-health-check

--- src/main/java/com/j2kb/minipetpee/health/HealthController.java
+++ src/main/java/com/j2kb/minipetpee/health/HealthController.java
@@ -2,9 +2,11 @@ package com.j2kb.minipetpee.health;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HealthController {
 
     @GetMapping("/aws/health")
