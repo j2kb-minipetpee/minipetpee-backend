f6665905ff3d223d46525e88a5b894e81d57f9cd
Dahee Goo <heeouo@kookmin.ac.kr>
2021-08-17 20:52:58 +0900

    <#18> refactor: FanCommentsResponse, RecentPostsResponse 제거 - 이전 리팩토링 PR 참고하여 코드 수정

--- src/main/java/com/j2kb/minipetpee/homepee/controller/HomepeeController.java
+++ src/main/java/com/j2kb/minipetpee/homepee/controller/HomepeeController.java
@@ -23,19 +23,17 @@ public class HomepeeController {
         RecentPostResponse post1 = new RecentPostResponse(1L, "안녕하새옹 보리애옹", LocalDateTime.now());
         RecentPostResponse post2 = new RecentPostResponse(2L, "오늘도 집사 개롭피기 성공", LocalDateTime.now());
         RecentPostResponse post3 = new RecentPostResponse(3L, "여름이었다", LocalDateTime.now());
-        List<RecentPostResponse> recentPostList = new ArrayList<>();
-        recentPostList.add(post1);
-        recentPostList.add(post2);
-        recentPostList.add(post3);
-        RecentPostsResponse recentPosts = new RecentPostsResponse(recentPostList);
+        List<RecentPostResponse> recentPosts = new ArrayList<>();
+        recentPosts.add(post1);
+        recentPosts.add(post2);
+        recentPosts.add(post3);
 
         // 공생평 조회
         FanCommentResponse fanComment1 = new FanCommentResponse(1L, 1011L, "콩이", "반갑개 맞팔 부탁하개~", LocalDateTime.now());
         FanCommentResponse fanComment2 = new FanCommentResponse(2L, 1013L, "춘식이", "울 집사 놀리는 것도 꿀잼이애옹", LocalDateTime.now());
-        List<FanCommentResponse> fanCommentsList = new ArrayList<>();
-        fanCommentsList.add(fanComment1);
-        fanCommentsList.add(fanComment2);
-        FanCommentsResponse fanComments = new FanCommentsResponse(fanCommentsList);
+        List<FanCommentResponse> fanComments = new ArrayList<>();
+        fanComments.add(fanComment1);
+        fanComments.add(fanComment2);
 
         // 홈피 리스폰스 dto 객체 생성
         FindHomepeeResponse homepeeResponse = new FindHomepeeResponse(profile, "gateImg1", recentPosts, "보리의 홈피애옹", 10, fanComments);
@@ -44,7 +42,10 @@ public class HomepeeController {
 
     // 공생평 작성
     @PostMapping("/fan-comments")
-    public ResponseEntity<FanCommentResponse> saveFanComment(@PathVariable("homepee-id") Long homepeeId, @RequestBody SaveFanCommentRequest fanCommentRequest) {
+    public ResponseEntity<FanCommentResponse> saveFanComment(
+            @PathVariable("homepee-id") Long homepeeId,
+            @RequestBody SaveFanCommentRequest fanCommentRequest
+    ) {
         // 추가된 공생평 데이터의 id
         Long id = 10L;
         Long memberId = fanCommentRequest.getMemberId();
@@ -58,14 +59,20 @@ public class HomepeeController {
 
     // 공생평 수정
     @PutMapping("/fan-comments")
-    public ResponseEntity<Void> updateFanComment(@PathVariable("homepee-id") Long homepeeId, @RequestBody UpdateFanCommentRequest fanCommentRequest){
+    public ResponseEntity<Void> updateFanComment(
+            @PathVariable("homepee-id") Long homepeeId,
+            @RequestBody UpdateFanCommentRequest fanCommentRequest
+    ){
         // 홈피 서비스: 공생평 수정 메서드 호출
         return ResponseEntity.noContent().build();
     }
 
     // 공생평 삭제
     @DeleteMapping("/fan-comments/{fan-comment-id}")
-    public ResponseEntity<Void> deleteFanComment(@PathVariable("homepee-id") Long homepeeId, @PathVariable("fan-comment-id") Long fanCommentId) {
+    public ResponseEntity<Void> deleteFanComment(
+            @PathVariable("homepee-id") Long homepeeId,
+            @PathVariable("fan-comment-id") Long fanCommentId
+    ) {
         // 홈피 서비스: 공생평 삭제 메서드 호출
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/j2kb/minipetpee/homepee/controller/dto/FindHomepeeResponse.java
+++ src/main/java/com/j2kb/minipetpee/homepee/controller/dto/FindHomepeeResponse.java
@@ -3,13 +3,15 @@ package com.j2kb.minipetpee.homepee.controller.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public class FindHomepeeResponse {
     private final ProfileResponse profile;
     private final String gateImageUrl;
-    private final RecentPostsResponse recentPosts;
+    private final List<RecentPostResponse> recentPosts;
     private final String title;
     private final int visitCount;
-    private final FanCommentsResponse fanComments;
+    private final List<FanCommentResponse> fanComments;
 }
