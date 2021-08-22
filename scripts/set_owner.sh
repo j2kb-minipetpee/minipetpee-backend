ae1bd43dbaac3c0357bbe79125a121333b9ffdd4
yoonjeong <cyj199637@gmail.com>
2021-08-22 22:28:23 +0900

    <chore>scripts

--- scripts/set_owner.sh
+++ scripts/set_owner.sh
@@ -8,4 +8,4 @@ cd /var/www/minipetpee-backend
 
 gradle build
 
-nohup java -jar /var/www/minipetpee-backend/build/libs/minipetpee-0.0.1-SNAPSHOT.jar &
+nohup java -jar /var/www/minipetpee-backend/build/libs/minipetpee-0.0.1-SNAPSHOT.jar 1>/dev/null 2>/dev/null &
