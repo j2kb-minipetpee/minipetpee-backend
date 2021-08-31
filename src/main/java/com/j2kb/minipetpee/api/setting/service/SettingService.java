430da8fd938df9b93591533a2b9394a987672360
Dahee Goo <heeouo@kookmin.ac.kr>
2021-08-31 23:58:14 +0900

    <#37> refactor: 코드리뷰 반영 - homepee & tab 유효성 검사 추가 및 메소드 추출

--- src/main/java/com/j2kb/minipetpee/api/setting/service/SettingService.java
+++ src/main/java/com/j2kb/minipetpee/api/setting/service/SettingService.java
@@ -16,7 +16,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Transactional
@@ -31,33 +34,54 @@ public class SettingService {
     // 프로필 조회 - 설정 가능한 계정 정보 및 탭 목록 조회
     @Transactional(readOnly = true)
     public Member findMemberByHomepeeId(Long homepeeId) {
-        Member member = memberRepository.findByHomepeeId(homepeeId)
+        Homepee homepee = findHomepeeById(homepeeId);
+        Member member = memberRepository.findByHomepeeId(homepee.getId())
                 .orElseThrow(() -> new ServiceException(HttpStatus.BAD_REQUEST, ErrorCode.EMP7001));
         return member;
     }
 
     public List<Tab> findTabsByHomepeeId(Long homepeeId) {
-        List<Tab> tabs = tabRepository.findAllByHomepeeId(homepeeId);
+        Homepee homepee = findHomepeeById(homepeeId);
+        List<Tab> tabs = tabRepository.findAllByHomepeeId(homepee.getId());
         return tabs;
     }
 
     // 프로필 변경
     public void updateSettings(Long homepeeId, UpdateSettingRequest settingRequest) {
-        Member member = memberRepository.findByHomepeeId(homepeeId)
+        Homepee homepee = findHomepeeById(homepeeId);
+        Member member = memberRepository.findByHomepeeId(homepee.getId())
                 .orElseThrow(() -> new ServiceException(HttpStatus.BAD_REQUEST, ErrorCode.EMP7001));
-        Homepee homepee = homepeeRepository.findById(homepeeId)
-                .orElseThrow(() -> new ServiceException(HttpStatus.BAD_REQUEST, ErrorCode.EMP7003));
         member.updateProfile(settingRequest.getProfile());
         homepee.update(settingRequest.getHomepee());
     }
 
     // 탭 공개 여부 변경
     public void updateTabs(Long homepeeId, UpdateTabsRequest updateTabsRequest){
+        // 홈피 확인
+        Homepee homepee = findHomepeeById(homepeeId);
+
+        // tab id Set 생성
+        Set<Long> tabIdSet = tabRepository.findAllByHomepeeId(homepee.getId())
+                .stream()
+                .map((tab) -> tab.getId())
+                .collect(Collectors.toSet());
+
         for (TabRequest tabRequest : updateTabsRequest.getTabs()){
+            // 홈피가 가진 탭이 맞는지 확인
+            if (!tabIdSet.contains(tabRequest.getId())){
+                new ServiceException(HttpStatus.BAD_REQUEST, ErrorCode.EMP7014);
+            }
             Tab tab = tabRepository.findById(tabRequest.getId())
-                    .orElseThrow(() -> new ServiceException(HttpStatus.BAD_REQUEST, ErrorCode.EMP7004));
+                    .orElseThrow(() -> new ServiceException(HttpStatus.NOT_FOUND, ErrorCode.EMP7004));
             tab.updateVisibility(tabRequest.isVisible());
         }
+    }
+
+    // 홈피 조회 (유효성 검사용)
+    public Homepee findHomepeeById(Long homepeeId) {
+        Homepee homepee = homepeeRepository.findById(homepeeId)
+                .orElseThrow(() -> new ServiceException(HttpStatus.NOT_FOUND, ErrorCode.EMP7003));
+        return homepee;
     }
 
 }
